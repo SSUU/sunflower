cfaa9f2fadb2da80c12dcbfb60f58ab1dc1aa1d1
Park YouJin <youjin881@gmail.com>
2015-06-08 18:33:18 +0900

    AWT - 쇼핑 카트
    - 화면을 객체로 만들어 사용
    - 화면에 관한 객체는 ch14.sub4.view 패키지에서 관리
    - 생성자, final, 배열, 메서드 오버로딩 사용

---
+++ ch14/sub4/ShoppingCart.java
@@ -0,0 +1,14 @@
+package ch14.sub4;
+
+import ch14.sub4.view.CartListView;
+
+public class ShoppingCart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new CartListView();
+
+	}
+
+}
---
+++ ch14/sub4/view/CartListView.java
@@ -0,0 +1,233 @@
+package ch14.sub4.view;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.LayoutManager;
+import java.awt.List;
+import java.awt.Panel;
+
+/*
+ * 해보기
+ * 1. 객체 정의
+ * - view에 대하여 필요한 상태와 행위를 객체(class)로 정의 할 수 있어야 함
+ * - 내가 만든 객체로 인스턴스 만들어보기 - new className();
+ * - 생성된 인스턴스를 변수에 담아 인스턴스가 가지고 있는 변수와 메소드 사용해보기
+ * - 인스턴스의 변수를 생성자를 통하여 초기화 해보기
+ * 2. final 사용
+ * - final 키워드를 사용할 줄 알아야 함
+ * - final 키워드가 의미하는 바가 무엇인가?
+ * 3. 배열 사용
+ * - 자바에서의 배열 사용법 알아야 함 
+ * - 배열의 생성, 인덱스 제어
+ * - 인스턴스 배열의 생성, 저장, 인데스 제어
+ * 4. Method Overloading(메소드 오버로딩)
+ * - Overloading 은 무엇인가?
+ * - Method Overloading 을 만드는 방법, 사용 방법, 규칙
+ * - 생성자도 Overloading 해보기
+ * 
+ */
+
+// CartListView 객체 정의
+public class CartListView {
+	// 각 Panel에게 final로 상수 선언
+	// 자주 사용하는 비교대상은 상수로 선언하여 사용하면 프로그램의 가독성과 유연성, 유지보수에 좋음
+	private final int P_TOP = 1;
+	private final int P_BODY = P_TOP+1;
+	private final int P_BOTTOM = P_BODY+1;
+	// 화면을 구성하는 도화지, Frame 선언
+	Frame f = null;
+
+	// 생성자를 통하여 초기화 작업	
+	public CartListView() {
+		init();
+	}
+	
+	// Frame에 대한 초기화를 수행하는 메소드
+	private void init() {
+		f = new Frame("쇼핑 카트 리스트");
+
+		initPanel();
+
+		f.setSize(250, 250);
+		f.setVisible(true);
+
+	}
+
+	// 각 Panel에 대한 초기화를 수행하는 메소드
+	// basePanel, topPanel, bodyPanel, bottomPanel 4개의 Panel을 배열을 사용하여 초기화
+	private void initPanel() {
+		// 각 Panel 적용할 Layout을 배열에 담아 사용
+		LayoutManager[] layout = { 
+				new BorderLayout(),	// basePanel
+				new BorderLayout(),	// topPanel
+				new BorderLayout(),	// bodyPanel
+				new GridLayout(1, 2)// bottomPanel 
+				};
+		// 각 Panel을 배열에 담아 사용
+		// 필요한 Panel은 layout의 개수와 같으므로 layout 배열의 length 만큼 생성
+		Panel[] panel = new Panel[layout.length];
+
+		// 각 Panel을 초기화 하는 반복문(for)
+		for (int i = 0; i < panel.length; i++) {
+			// 배열에 Panel 인스턴스를 생성하여 담음
+			panel[i] = new Panel();
+			// 각 Panel에 사용할 layout을 설정
+			panel[i].setLayout(layout[i]);
+
+			// 첫번째 Panel은 Frame에 추가
+			if (i == 0) {
+				f.add(panel[i]);
+			}
+			// 나머지 Panel은 basePanel에 추가
+			// panel[0] : basePanel
+			// panel[1] : topPanel
+			// panel[2] : bodyPanel
+			// panel[3] : bottomPanel
+			else {
+				// BorderLayout의 위치를 담기 위한 변수
+				String position = "";
+				Color c = null;
+				switch (i) {
+				case P_TOP: // 현재 Panel이 TOP Panel일 경우 BorderLayout의 위치를 NORTH로 설정
+					// 배경색은 RED
+					position = BorderLayout.NORTH;
+					c = Color.red;
+					break;
+				case P_BODY:// 현재 Panel이 BODY Panel일 경우 BorderLayout의 위치를 CENTER로 설정
+					// 배경색은 BLACK
+					position = BorderLayout.CENTER;
+					c = Color.black;
+					break;
+				case P_BOTTOM:// 현재 Panel이 BOTTOM Panel일 경우 BorderLayout의 위치를 SOUTH로 설정
+					// 배경색은 GREEN
+					position = BorderLayout.SOUTH;
+					c = Color.green;
+					break;
+
+				default:
+					break;
+				}
+				// 각 Panel에 배경색을 설정
+				panel[i].setBackground(c);
+				// basePanel에 각 Panel을 position 위치로 추가
+				panel[0].add(panel[i], position);
+				// 각 Panel에 필요한 Component을 추가하는 메소드
+				initComponent(panel[i], i);
+				
+			}
+
+		}
+
+	}
+
+	// 각 Panel에 대하여 Component를 추가하는 메소드
+	// initComponent() 메소드는 현재의 Panel의 위치에 따라 switch문으로 Component를 구분하여 추가하는 수행하는 메소드
+	private void initComponent(Panel p, int i) {
+		// 각 Panel (top, body, bottom)에 맞는 Component 추가를 위한 분기문 
+		switch (i) {
+		case P_TOP: // TOP Panel의 Component 추가 수행 메소드
+			addTopComponent(p);
+			break;
+		case P_BODY: // BODY Panel의 Component 추가 수행 메소드
+			addBodyComponent(p);
+			break;
+		case P_BOTTOM: // BOTTOM Panel의 Component 추가 수행 메소드
+			addBottomComponent(p);
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+	// TOP Panel에 필요한 Component만을 추가하는 메소드 
+	private void addTopComponent(Panel p) {
+		// 2차 배열의 사용법 익히기 
+		String[][] name = { { "<", ">" }, { "쇼핑 목록" } };
+		String[] btnPosi = {BorderLayout.WEST, BorderLayout.EAST};
+		String[] lbPosi = {BorderLayout.CENTER};
+		Button[] btn = new Button[name[0].length];
+		Label[] lb = new Label[name[1].length];
+
+		// 배열의 크기만큼 Component를 추가
+		// 반복적으로 Component를 추가하기 때문에 반복문 for을 사용
+		for (int i = 0; i < name.length; i++) {
+			for (int j = 0; j < name[i].length; j++) {
+				if (i == 0) {
+					btn[i] = new Button(name[i][j]);
+					addComponent(p, btn[i], btnPosi[j]);
+				}else{
+					lb[j] = new Label(name[i][j]);
+					addComponent(p, lb[j], lbPosi[j]);
+				}
+			}
+		}
+
+	}
+	
+	// BODY Panel에 필요한 Component만을 추가하는 메소드
+	private void addBodyComponent(Panel p){
+		
+		String[] name = {"Body"};
+		String[] btnPosi = {BorderLayout.CENTER};
+		Button[] btn = new Button[name.length];
+		
+		for(int i=0; i<name.length; i++){
+			btn[i] = new Button(name[i]);
+			addComponent(p, btn[i], btnPosi[i]);
+		}
+		
+	}
+	
+	// BOTTOM Panel에 필요한 Component만을 추가하는 메소드
+	private void addBottomComponent(Panel p){
+		
+		String[] name = {"주문","취소"};
+//		String[] btnPosi = {BorderLayout.CENTER};
+		Button[] btn = new Button[name.length];
+		
+		for(int i=0; i<btn.length; i++){
+			btn[i] = new Button(name[i]);
+			addComponent(p, btn[i]);
+		}
+		
+	}
+
+	// Method Overloading - addComponent
+	// Panel에 Component를 추가하는 메소드
+	// Button 이라는 Component를 추가하는 메소드
+	// Layout이 BorderLayout이기 때문에 추가할 Component의 위치 값이 필요함(posi)
+	private void addComponent(Panel p, Button btn, String posi) {
+
+		p.add(btn, posi);
+
+	}
+	// Panel에 Component를 추가하는 메소드
+	// Button 이라는 Component를 추가하는 메소드
+	// Layout이 GridLayout이기 때문에 추가할 Component의 위치 값이 필요 없음
+	private void addComponent(Panel p, Button btn) {
+		
+		p.add(btn);
+		
+	}
+	// Layout이 BorderLayout이기 때문에 추가할 Component의 위치 값이 필요함(posi)
+	private void addComponent(Panel p, Label lb, String posi) {
+		
+		p.add(lb, posi);
+		
+	}
+	// List 이라는 Component를 추가하는 메소드
+	// Layout이 BorderLayout이기 때문에 추가할 Component의 위치 값이 필요함(posi)
+	private void addComponent(Panel p, List list, String posi) {
+		
+		p.add(list, posi);
+		
+	}
+	// end of Method Overloading - addComponent
+
+}
