96ec1289d4987e2c9624db6ee149df9d729674fb
Park YouJin <youjin881@gmail.com>
2015-10-15 17:55:54 +0900

    5차 방정보 가져오기
    - TorronCont에서 DB조회후 list.js에 방 정보 전달 기능 수정보

--- 토론/박유진/com/kh/torront/controller/TorronCont.java
+++ 토론/박유진/com/kh/torront/controller/TorronCont.java
@@ -1,6 +1,12 @@
 package com.kh.torront.controller;
 
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,51 +17,29 @@ import org.springframework.orm.ibatis.SqlMapClientTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kh.torront.vo.DatgleVO;
 import com.kh.torront.vo.ListVO;
-import com.kh.torront.vo.Naljja;
-
-
 
 @Controller
 public class TorronCont {
 	
+	double page = 0;
 	@Autowired
 	SqlMapClientTemplate sqlMapClientTemplate;
 	
-	@RequestMapping("view/nalja.lip")
-	public void nalja(Naljja nal,HttpServletRequest request){
-		//System.out.println(nal.getNalja());
-		String nalja = request.getParameter("nalja");
-		String rdo = request.getParameter("tfinish");
-		System.out.println(nalja);
-		System.out.println(rdo);
-		System.out.println(nal.getNalja());
-		System.out.println(nal.getTfinish());
-//		sqlMapClientTemplate.insert("naljja.insert",nal);
-	}//
-	
 	@RequestMapping("view/torron/tcreate.lip")
 	public void create(HttpServletRequest request, HttpServletResponse response, ListVO LVO ){
-	
 		System.out.println("제목: "+LVO.getTsub());
-		//����
 		System.out.println("내용 : "+LVO.getTcon());
-		//����
-		System.out.println("카테고리 : "+LVO.getCateno());
 		//url
-		
-		System.out.println("url!! : "+ LVO.getScrap().trim());
 		String url = LVO.getScrap().trim();
-		
+		//x자르기
 		String[] urlValues = url.split("X");
 		for(int i = 0 ; i < urlValues.length ; i++){
-			System.out.println("url"+i+":" + urlValues[i].trim() );
+			System.out.println("url"+i+":" + urlValues[i].trim());
 		}
-		
-		
 		String cate = LVO.getCateno();
-		System.out.println("카테고리 : "+LVO.getCateno());
-		
 		if(cate.equals("pol")){
 			cate = "1";
 		}else if(cate.equals("spo")){
@@ -64,16 +48,18 @@ public class TorronCont {
 			cate = "3";
 		}
 		//DateFormat df = new DateFormat();
-		System.out.println("cate :"+cate);
-		//ī�װ?
+		System.out.println("카테고리넘버 :"+cate);
 		String a = LVO.getCateno();
-		System.out.println("카테고리 넘버 : "+LVO.getTfinish());
+		System.out.println("마감날짜 : "+LVO.getTfinish());
 		String t = LVO.getTfinish();
-		String t1 = t.substring(0,3);
-		String t2 = t.substring(4,5);
-		String t3 = t.substring(8,10);
-	
-		System.out.println(t3 + "-" + t1 + "-" + t2);
+		LVO.setTtimer(t);
+		String [] list = t.split("\\s+");
+		for(int i = 0 ; i < list.length ; i++){
+			System.out.println(list[i]);
+		}
+		String t1 = list[0];
+		String t2 = list[2].substring(2,4);
+		String t3 = list[1];
 		//마감시간
 		 switch (t1) {
 		    case "jan" : t1 = "1"; break;
@@ -88,19 +74,220 @@ public class TorronCont {
 		    case "oct" : t1 = "10"; break;
 		    case "nov" : t1 = "11"; break;
 		    case "dec" : t1 = "12"; break;
-		    
-		   
 		  }
 	
-		 System.out.println(t3 + "-" + t1 + "-" + t2);
 		 String s = "/";
-		 String tf = t3 +s+ t1 +s+ t2; 
+		 String tf = t2 +s+ t1 +s+ t3;
+		LVO.setTfinish(tf);
+		LVO.setCateno(cate);
+		System.out.println(tf);
+		sqlMapClientTemplate.insert("torron.insert",LVO);
+		//날짜 ??/??/??
+		PrintWriter out;
+		try {
+			out = response.getWriter();
+			out.print("1");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		 
-		 List<ListVO> list = sqlMapClientTemplate.queryForList("torron.select");
-		 for(int i=0; i<list.size(); i++){
-			 ListVO vo = list.get(i);
-			 System.out.println("1...."+vo.getMemail());
-			 
-		 }
+//		 List<ListVO> list = sqlMapClientTemplate.queryForList("torron.select");
+//		 
 	}
+////////////////////정호
+	@RequestMapping("paging.lip")
+	public void paging(HttpServletRequest request,HttpServletResponse response,ListVO lvo){
+	System.out.println("여기도 왔다.");
+	String a = (String)sqlMapClientTemplate.queryForObject("torron.pagecnt");
+	double b = Integer.parseInt(a);			
+	page = b;
+		double pageno = Math.ceil(page/10);
+			if(pageno == 0 ){
+				pageno = 1;
+				}
+				PrintWriter out;
+				try {
+					out = response.getWriter();
+					out.print(pageno);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+}/////page
+	//////
+	@RequestMapping("select.lip")
+	public void sresult(HttpServletResponse response,ListVO lvo, HttpServletRequest request){
+		System.out.println("왔다.");
+		PrintWriter out;
+		try {
+			 List<ListVO> list = sqlMapClientTemplate.queryForList("torron.select");
+			 ArrayList<HashMap<String, String>> al = new ArrayList<HashMap<String,String>>();
+			 HashMap<String, String> map = null;
+			 ////////////
+			 String no = request.getParameter("pno");
+				int number = 1;
+				if(no == null){
+					
+				}else{
+					number = Integer.parseInt(no);
+				}
+				int exmax = 9;
+				int exmin = 0;
+				
+				exmax = (number * 10)-1 ;
+				exmin = (exmax - 9);
+				
+				int pa = (int)page;
+				
+				if(pa < exmax && exmax%pa != 0 ){
+					int eee = exmax - pa;
+					exmax = exmax - eee-1;
+				}
+				System.out.println(number);
+				System.out.println(pa);
+				System.out.println(exmin);
+				System.out.println(exmax);
+				
+			 ///////////
+			 for(int i=exmin; i <= exmax; i++){
+				 map = new HashMap<String, String>();
+				 map.put("tno", list.get(i).getTno());
+				 map.put("ting",list.get(i).getTing());
+				 map.put("memail", list.get(i).getMemail());
+				 map.put("tsub", list.get(i).getTsub());
+				 map.put("tdate", list.get(i).getTdate());
+				 map.put("mnic", list.get(i).getMnic());
+				 
+				 al.add(map);
+			 }
+			 response.setCharacterEncoding("UTF-8");
+			 request.setCharacterEncoding("UTF-8");
+			 
+			ObjectMapper mapper = new ObjectMapper();
+			out = response.getWriter();
+			out.print(mapper.writeValueAsString(al));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	@RequestMapping("room.lip")
+	public String room_a(HttpServletRequest request, HttpServletResponse response){
+		System.out.println("1");
+		return "torron/room.jsp";
+	}
+	
+	String boardno ="";
+	@RequestMapping("rcontents.lip")
+	public void roomcontents(HttpServletRequest request, ListVO lvo, HttpServletResponse response,DatgleVO dvo){
+		HashMap<String, Object> map = null;
+		try {
+			request.setCharacterEncoding("UTF-8");
+			response.setCharacterEncoding("UTF-8");
+			boardno = request.getParameter("nono");
+
+			lvo.setTno(boardno);
+			dvo.setTno(boardno);
+			
+			List<ListVO> list = sqlMapClientTemplate.queryForList("torron.boardno",lvo);
+			List<DatgleVO> datlist = sqlMapClientTemplate.queryForList("torron.datcon",dvo);
+			
+			System.out.println("list : "+list.get(0).getTsub());
+			
+			map = new HashMap<String, Object>();
+			ArrayList<HashMap<String, String>> al=null;
+			al  = new ArrayList<HashMap<String,String>>();
+				//map.put("datli", datlist.size()+"");
+			
+			if(list != null){
+				map.put("list", list);
+			}
+			if(datlist != null){
+				map.put("datlist", datlist);
+			}
+//				if(datlist.size() > 0 ){
+//					for(int j=0 ;j < datlist.size();j++){
+//						map = new HashMap<String, String>();
+//						map.put("ttimer",list.get(0).getTtimer());
+//						map.put("tsub", list.get(0).getTsub());
+//						map.put("tcon", list.get(0).getTcon());
+//						map.put("mnic", list.get(0).getMnic());
+//						map.put("datcon", datlist.get(j).getTdatcon());
+//						map.put("tdatno", datlist.get(j).getTdatno());
+//						al.add(map);
+//					}//for
+//					
+//				}else{
+//					al.add(map);
+//				}//else
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		ObjectMapper obj = new ObjectMapper();
+		try {
+			PrintWriter out = response.getWriter();
+			out.print(obj.writeValueAsString(map));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}//roomcontents
+	
+	//댓글 insert 부분
+	@RequestMapping("datglelist.lip")
+	public void datglelist(HttpServletRequest request,ListVO lvo,HttpServletResponse response){
+			try {
+				request.setCharacterEncoding("UTF-8");
+				response.setCharacterEncoding("UTF-8");
+				String no =request.getParameter("hidno");
+				String txt = URLDecoder.decode(request.getParameter("hidtxt"), "UTF-8");
+				lvo.setTno(boardno);
+				lvo.setTcon(txt);
+				sqlMapClientTemplate.insert("torron.datglelist",lvo);
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+	}//datglelist
+	@RequestMapping("datupdate.lip")
+	public void datupdate(HttpServletRequest request,HttpServletResponse response,DatgleVO dvo){
+		
+		PrintWriter out;
+		try {
+			
+			String tdatno = request.getParameter("delcontents");
+			System.out.println(tdatno);
+			dvo.setTdatno(tdatno);
+			int res = sqlMapClientTemplate.delete("torron.datdelete",dvo);
+			System.out.println(res);
+			out = response.getWriter();
+			out.print(res);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}//datupdate
+	@RequestMapping("tingupdate.lip")
+	public void tingupdate(HttpServletRequest request,HttpServletResponse response,ListVO lvo){
+		String boardno = request.getParameter("boardno");
+		lvo.setTno(boardno);
+		String a = (String)sqlMapClientTemplate.queryForObject("torron.sboard",lvo);
+		if(a.equals("진행중")){
+			int tingres = sqlMapClientTemplate.update("torron.updateting",lvo);
+			System.out.println(tingres);
+			if(tingres == 1){
+				System.out.println("ting 가 마감으로 수정되었습니다.");
+			}//if
+		}
+	}//tingupdate
 }
--- 토론/박유진/js/torron/list.js
+++ 토론/박유진/js/torron/list.js
@@ -1,81 +1,378 @@
-$(document).ready(function() {
-	// dao list 정보들을 받아와서 
-	listEvent();
-	
-});
+$(document).ready(
+		function() {
+			$.ajax({
+				url : "../select.lip",
+				type : "get",
+				success : function(result) {
+					$("#listmenu").text("");
+					var tata = JSON.parse(result);
+					var a = new Array();
+					a = tata;
+					
+					if (tata.length > 0) {
+						var tableTr = "<tr>";
+						for (var i = 0; i < tata.length; i++) {
+							tableTr += "<td align='center'>" + a[i].tno
+									+ "</td>"
+									+ "<td align='center'><label id='"
+									+ a[i].tno + "'>" + a[i].tsub
+									+ "</label></td>" + "<td align='center'>"
+									+ a[i].mnic + "</td>"
+									+ "<td align='center'>"
+									+ a[i].tdate.substring(0, 10) + "</td>"
+									+ "<td align='center'>" + a[i].ting
+									+ "</td></tr>";
+						}
+						$("#listmenu").append(tableTr);
+					}// if
 
+					boardclick();
+					listEvent();
 
+				} // success!
+			});// ajax
+			
+///////////////////////////////정호
+			$.ajax({
+				url:"../paging.lip",
+				type:"get",
+				success: function(result){
+					var result1 = JSON.parse(result);
+					$("#pageno").text("");
+					for(var i = 1; i <= result; i++){
+						var w = "<a href='' onclick='atag(); return false' >"+i+"</a>"
+						$("#pageno").append("["+w+"]&nbsp");
+					}
+					//////
+					$("#pageno > a").click(function(){
+						var a = $(this).text();
+						atag(a);
+					});
+					////////
+				}//success
+			
+			});//ajax
+			
+		});///function
+/////////////////////////////////정호
+		//a태그 클릭시
+		function atag(pageno){
+		
+		if(pageno == null){
+		
+		
+		}else{
+		$.ajax({
+		url:"../select.lip",
+		type : "get",
+		data : {pno : pageno},
+		success :function(result){
+		$("#listmenu").text("");
+		var sub = JSON.parse(result);
+		var a = new Array();
+		a = sub;
+		
+		if (sub.length > 0) {
+			var tableTr = "<tr>";
+			var listno = sub.length;
+			$("#listmenu").text("");
+			for (var i = 0; i < sub.length; i++) {
+				tableTr += "<td align='center'>" + a[i].tno
+						+ "</td>"
+						+ "<td align='center'><label id='"
+						+ a[i].tno + "'>" + a[i].tsub
+						+ "</label></td>" + "<td align='center'>"
+						+ a[i].mnic + "</td>"
+						+ "<td align='center'>"
+						+ a[i].tdate.substring(0, 10) + "</td>"
+						+ "<td align='center'>" + a[i].ting
+						+ "</td></tr>";
+			}
+			$("#listmenu").append(tableTr);
+		}// if
+		boardclick();
+		listEvent();
+		}
+		});
+		}
+		}
+//////////////////////////////////
 
-
-function listEvent(){
-	
-	$("#write").click(function(){
-		var myWindow = window.open(tPath.getView("create"),
-				"create","width=500, height=700","scrollbar=no","toolbar=no");
+var dlistno=0;
+//보드 리스트 클릭했을시,
+function boardclick() {
+	// 게시판 넘버 가져온다.
+var datlength=0;
+var datcon = null;
+	$("td > label").click(function() {
+		var boardno = $(this).attr("id");
+		alert("boardno : "+boardno);
+		$.ajax({
+			url : "../rcontents.lip",
+			type : "get",
+			cache : false,
+			data : {
+				nono : boardno
+			},
+			success : function(result) {
+				$("#listmenu").text("");
+				var rjson = JSON.parse(result);
+				var roomcon = new Array();
+				
+				var list = rjson.list;
+//				alert("list : "+list.length);
+//				for(var i=0; i<list.length; i++){
+//					for(var key in list[i]){
+//						alert("key : "+key+", v : "+list[i][key]);
+//					}
+//				}
+				var datlist = rjson.datlist;
+//				alert("dat : "+datlist.length);
+//				for(var i=0; i<datlist.length; i++){
+//					for(var key in datlist[i]){
+//						alert("key : "+key+", v : "+datlist[i][key]);
+//					}
+//				}
+				
+				roomcon = rjson;
+				$(".contents").load(tPath.getView("room"), function() {
+					$(".head > label").text(list[0].tsub);
+					$(".t_contents > label").text(list[0].tcon);
+					$("#hidtimer").val(list[0].ttimer);
+					$(".t_host > label").text(list[0].mnic);
+					$("#boardnumber > label").text(boardno);
+					/////////////////////////////////
+					var no=0;
+					for(var i=0;i<datlist.length;i++){
+						 var ss = "<fieldset id='"+no+"'><label class ='"+no+"' id='"+datlist[i].tdatno+"'>"+datlist[i].tdatcon+"</label>&nbsp;&nbsp;<button class='ox' id='"+no+"'>X</button><br/><button class='singo' id='sin"+no+"'>신고하기</button></fieldset>";
+							$("#c3").append(ss);
+							no++;
+					}
+					
+					$(".ox").click(function(){
+						var cno = $(this).attr("id");
+						alert(cno);
+						
+						//댓글삭제 유효성해야함.
+						var delcon = $("label[class='"+cno+"']").attr("id");
+						$.ajax({
+							url:"../datupdate.lip",
+							type:"get",
+							data:{delcontents:delcon},
+							cache:false,
+							success:function(result){
+								alert(result);
+								$("fieldset[id='"+cno+"']").remove();
+							}//function
+						})//ajax
+					
+					});
+						////////
+					//마감입니다. 로 변경하는 부분.
+					var boardnum = $("#boardnumber > label").text();
+					$.ajax({
+						url:"../tingupdate.lip",
+						type:"get",
+						data:{boardno:boardnum},
+						success:function(){
+							
+						}
+					})//ajax
+				});//room load
+				
+				
+				
+			}
+		});
+			
+		
 	});
-	
+	// 게시판 리스트 호버.
+	$("td > label").hover(function() {
+		$(this).css("color", "red");
+	}, function() {
+		$(this).css("color", "black");
+	});
+
+}// 보드 리스트 액션 관리
+
+function listEvent() {
+	$("#write").click(
+			function() {
+				var myWindow = window.open(tPath.getView("create"), "create",
+						"width=500, height=700", "scrollbar=no", "toolbar=no");
+			});
 }
 
-//<tr>
-//<td align="center" ><font size="6">글번호</td>
-//<td align="center"><font size="6">제목</td>
-//<td align="center"><font size="6">작성자</td>
-//<td align="center"><font size="6">작성일</td>
-//<td align="center"><font size="6">진행상황</td>
-//</tr>
-
-// 방 개설시 추가	
-// data 에서 추가 정보 받아와 추가하기
-
-function addRoom(data){
-	
-	var newTr = "<tr>";
-	var newTd = "";
-	
-	for(var i=0; i<5; i++){
-		var addData = addTableData(i, data);
-		newTd += "<td align='center' id="+addData.id+">"+addData.value+"</td>";
-	}
-	
-	newTr += newTd+"</tr>";
-	alert("d2 : "+newTr);
-	
-	$("#t_listTable").append(newTr);
-}
-
-function addTableData(idx, data){
-	
+function addTableData(idx, data) {
 	var addData = {
-			id : "",
-			value : ""
+		id : "",
+		value : ""
 	}
-	
+
 	switch (idx) {
 	case 0:
-		addData.id = "no"+idx;
+		addData.id = "no" + idx;
 		addData.value = idx;
 		break;
 	case 1:
-		addData.id = "sub"+idx;
+		addData.id = "sub" + idx;
 		addData.value = data.subject;
 		break;
 	case 2:
-		addData.id = "host"+idx;
+		addData.id = "host" + idx;
 		addData.value = data.host;
 		break;
 	case 3:
-		addData.id = "date"+idx;
+		addData.id = "date" + idx;
 		addData.value = '111-111';
 		break;
 	case 4:
-		addData.id = "n"+idx;
+		addData.id = "n" + idx;
 		addData.value = 'nnn';
 		break;
 
 	default:
 		break;
 	}
-	
+
 	return addData;
+}
+
+
+/////////////////////////////////////////timer
+var bubu;
+var newtime;
+function reverse_counter(){
+	/////////////////////////
+	today = new Date();
+	var ss = $("#hidtimer").val();
+	d_day = new Date(ss);
+	if(d_day - today > 0){
+		days = (d_day - today) / 1000 / 60 / 60 / 24;
+		daysRound = Math.floor(days);
+		hours = (d_day - today) / 1000 / 60 / 60 - (24 * daysRound);
+		hoursRound = Math.floor(hours);
+		minutes = (d_day - today) / 1000 /60 - (24 * 60 * daysRound) - (60 * hoursRound);
+		minutesRound = Math.floor(minutes);
+		seconds = (d_day - today) / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) -
+		(60 * minutesRound);
+		secondsRound = Math.round(seconds);
+		sec = " 초."
+		min = " 분, "
+		hr = " 시간, "
+		dy = " 일, "
+		document.counter.counter_box.value = " 마감시간 : " + daysRound + 
+		dy + hoursRound + hr + minutesRound + min + secondsRound + sec;
+	}//if
+		if(d_day - today < 0){
+			document.counter.counter_box.value = "마감이야";
+			window.clearInterval(newtime);
+			window.clearTimeout(newtime);
+			$(".tbtn").attr("disabled","disabled");
+			$(".ta").attr("disabled","disabled");
+			$(".ta").attr("placeholder","마감되었습니다.");
+			$(".cbtn").text("마감되었습니다.");
+			$(".cbtn").css("font-size","x-large");
+//			var boardnum = $("#boardnumber > label").text();
+//			$.ajax({
+//				url:"../tingupdate.lip",
+//				type:"get",
+//				data:{boardno:boardnum},
+//				success:function(){
+//				}
+//			})//ajax
+			
+		}//if
+	
+	}
+////////////////////////////////////////////시간세팅
+$(document).ready(function(){
+	//newtime = window.set("reverse_counter();", 1000);
+	newtime = window.setInterval("reverse_counter();", 1000)
+});
+
+
+function stt(dday){
+	test = new Date();
+	var month =  test.getMonth()+1;
+	var day =  test.getDate();
+	var hour = test.getHours();
+	var minutes = test.getMinutes();
+	var second = test.getSeconds();
+	var fullyear = test.getFullYear();
+	var mon ="";
+	var daytotal ="";
+	//document.counter.c2.value = test.getFullYear()+""+month+""+day+""+test.getTime();
+	switch(month){
+	case 1: 
+		mon = "jan";
+		daytotal = "31";
+	break;
+	case 2: 
+		mon ="feb";
+		daytotal = "28";
+	break;
+	case 3: 
+		mon ="mar";
+		daytotal = "31";
+	break;
+	case 4: 
+		mon ="apr";
+		daytotal = "30";
+	break;
+	case 5: 
+		mon ="may";
+		daytotal = "31";
+	break;
+	case 6: 
+		mon ="jun";
+		daytotal = "30";
+	break;
+	case 7: 
+		mon ="jul";
+		daytotal = "31";
+	break;
+	case 8: 
+		mon ="aug";
+		daytotal = "31";
+	break;
+	case 9: 
+		mon ="sep";
+		daytotal = "30";
+	break;
+	case 10: 
+		mon ="oct";
+		daytotal = "31";
+	break;
+	case 11: 
+		mon ="nov";
+		daytotal = "30";
+	break;
+	case 12: 
+		mon ="dec";
+		daytotal = "31";
+	break;
+	}
+	
+	var plusday;
+	if(dday == 3){
+		plusday = (day+3);
+	}else if(dday == 7){
+		plusday = (day+7);
+	}else if(dday == 10){
+		plusday = (day+10);
+	}
+	
+	var dday = "";
+	var afterday ="";
+	if(plusday > daytotal){
+		dday = plusday - daytotal;
+		afterday = mon+" "+dday+" "+fullyear+" "+hour+":"+minutes+":"+second;
+	}else{
+		afterday = mon+" "+plusday+" "+fullyear+" "+hour+":"+minutes+":"+second;
+	}
+	//reverse_counter().gogo.val  = afterday;
+	return afterday;
 }
 
